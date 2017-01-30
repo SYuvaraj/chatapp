eed2950b4ae2c886d485e987c9785014539868a4
Yuvaraj <yuvaraj@mantralabsglobal.com>
2017-01-31 00:40:06 +0530

    ibot predictive response

--- app.js
+++ app.js
@@ -2,53 +2,30 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var fs = require('fs');
 var path = require('path');
-
-//Define the port to listen
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-var users=[];
-
-
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile('bot.html');
 });
-
-
-
-//Whenever someone connects this gets executed
+users = [];
 io.on('connection', function(socket){
   console.log('A user connected');
-
-  socket.on("newuser", function(data){
-  	users.push(data);
-  	console.log(users);
-  	if( users.username === data.username && users.pass === data.pass){}
-  	app.get('/index.html', function(req,res){
-		
-  	});
-
+  socket.on('setUsername', function(data){
+    console.log(data);
+    if(users.indexOf(data) > -1){
+      socket.emit('userExists', data + ' username is taken! Try some other username.');
+    }
+    else{
+      users.push(data);
+      socket.emit('userSet', {username: data});
+    }
   });
-
-
-  //Whenever someone disconnects this piece of code executed
-  socket.on('disconnect', function () {
-    console.log('A user disconnected');
-  });
-
+  socket.on('msg', function(data){
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+  })
 });
-
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+http.listen(3000, function(){
+  console.log('listening on localhost:3000');
 });
-
-
-
